30e20bfa2c49fecf914e87199c9af99462275bbf
trondeh80 <trond.erling@gmail.com>
2017-04-16 15:42:04 +0200

    Added menu functionality. Added scss to match how the menu will work.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+(function(){
+  $(document).ready(documentReady);
+
+  function documentReady(){
+    $('.menu-icon').click(toggleMenu);
+    $(document).click(closeMenu);
+    $('.menu-icon-close').click(closeMenu);
+    $('header nav').click(function(e){
+        e.stopPropagation();
+    });
+  }
+
+  function closeMenu(){
+    $('.menuitems').removeClass('is-active');
+    $('#overlay').removeClass('is-active');
+  }
+
+  function toggleMenu(){
+    $('.menuitems').toggleClass('is-active');
+    $('#overlay').toggleClass('is-active');
+  }
+
+})();
