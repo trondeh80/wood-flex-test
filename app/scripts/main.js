dc8a1b5a684aedc474bd5b6c63c200f0fc9ba250
trondeh80 <trond.erling@gmail.com>
2017-05-05 20:00:35 +0200

    Added better form, and nicer footer. Added test pages for article, mainpage and contact page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,17 +7,56 @@
   $(document).ready(documentReady);
 
   function documentReady(){
+    addEvents();
+    setupAnimation();
+    setupAnalytics();
+  }
+
+  function addEvents(){
     $('.menu-icon').click(toggleMenu);
     $('.overlay').click(closeMenu);
     $('.menu-icon-close').click(closeMenu);
-    setupAnimation();
-    setupAnalytics();
     $('.email').html(getEmail());
+    $('.email-link').attr('href', `mailto:${getEmail()}`);
+    $('form').each(function(){
+      addFormValidation($(this));
+    });
+  }
+
+  function isValidEmail(emailAddress){
+    const reggie = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reggie.test(emailAddress);
+  }
+
+  function addFormValidation(form){
+    $('input, textarea', form).focus(function(){
+      $(this).addClass('touched');
+      form.addClass('touched');
+    });
+    let formIsValid = true;
+    $('input, textarea', form).blur(function(){
+      let isValid = false;
+      let value = $(this).val();
+      let inputType = $(this).attr('type');
+      if (value.length) {
+        if (inputType === 'email'){
+          isValid = isValidEmail(value);
+        } else {
+          isValid = true;
+        }
+      }
+      $(this).toggleClass('valid', isValid);
+      $(this).toggleClass('invalid', !isValid);
+      if (!isValid){
+        formIsValid = false;
+      }
+
+      form.toggleClass('valid', formIsValid);
+    });
   }
 
   function getEmail(){
-    const domain = 'b-wood.no';
-    return `bge@${domain}`;
+    return String.fromCharCode(98, 103, 101, 64, 98, 45, 119, 111, 111, 100, 46, 110, 111);
   }
 
   function closeMenu(){
