d12760b28a5e16a5f37099620cbf60739f755d2b
Trond Erling Hundal <trond.erling@gmail.com>
2018-03-04 02:45:52 +0100

    Add gallery lightbox

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
-(function(){
+(function () {
 
   const $ = window.jQuery;
+  const $$ = window.document.querySelectorAll.bind(window.document);
+  let galleryModel = [];
 
   const BWOOD = {
     ga: 'UA-1139355-43'
@@ -8,40 +10,79 @@
 
   $(document).ready(documentReady);
 
-  function documentReady(){
+  function documentReady() {
     addEvents();
     setupAnimation();
     setupAnalytics();
+
+    if ($('.gallery, .image-preview').length) {
+      // Check url if image is selected
+      // routeToImage...
+      setupLightbox(); // attach events
+    }
   }
 
-  function addEvents(){
+  function setupLightbox() {
+    galleryModel = [];
+    $('#overlay, #close-gallery-btn').click(closeLightbox);
+    $('.gallery').each(function() {
+      $('.gallery-item', $(this)).each(function() {
+        const linkItem = $('a', $(this));
+        galleryModel.push(linkItem);
+        linkItem.click(getSingleImageClickEvent(linkItem.attr('href'), $(this), galleryModel.length));
+      });
+    });
+  }
+
+  function closeLightbox() {
+    $('#overlay').removeClass('open');
+    // history.pushState(null, null, null);
+  }
+
+  // Get click listener
+  function getSingleImageClickEvent(image, galleryItem, index) {
+    return (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      // history.pushState(null, null, `#image-${index}`);
+      setLightboxImage(image);
+    };
+  }
+
+  function setLightboxImage(image) {
+    const img = `<img class="lb-image fullsize-image" src="${image}" />`;
+    $('.modal > .modal-inner > .content').html(img);
+    $('#overlay').toggleClass('open');
+  }
+
+  function addEvents() {
     $('.menu-icon').click(toggleMenu);
     $('.overlay').click(closeMenu);
     $('.menu-icon-close').click(closeMenu);
     $('.email').html(getEmail());
     $('.email-link').attr('href', `mailto:${getEmail()}`);
-    $('form').each(function(){
+    $('form').each(function () {
       addFormValidation($(this));
     });
   }
 
-  function isValidEmail(emailAddress){
+  function isValidEmail(emailAddress) {
     const reggie = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return reggie.test(emailAddress);
   }
 
-  function addFormValidation(form){
-    $('input, textarea', form).focus(function(){
+  function addFormValidation(form) {
+    $('input, textarea', form).focus(function () {
       $(this).addClass('touched');
       form.addClass('touched');
     });
     let formIsValid = true;
-    $('input, textarea', form).blur(function(){
+    $('input, textarea', form).blur(function () {
       let isValid = false;
       let value = $(this).val();
       let inputType = $(this).attr('type');
       if (value.length) {
-        if (inputType === 'email'){
+        if (inputType === 'email') {
           isValid = isValidEmail(value);
         } else {
           isValid = true;
@@ -49,7 +90,7 @@
       }
       $(this).toggleClass('valid', isValid);
       $(this).toggleClass('invalid', !isValid);
-      if (!isValid){
+      if (!isValid) {
         formIsValid = false;
       }
 
@@ -57,24 +98,24 @@
     });
   }
 
-  function getEmail(){
+  function getEmail() {
     return String.fromCharCode(98, 103, 101, 64, 98, 45, 119, 111, 111, 100, 46, 110, 111);
   }
 
-  function closeMenu(){
+  function closeMenu() {
     $('.menu-activate').removeClass('is-active');
     $('#overlay').removeClass('is-active');
   }
 
-  function toggleMenu(){
+  function toggleMenu() {
     $('.menu-activate').toggleClass('is-active');
     $('#overlay').toggleClass('is-active');
   }
 
-  function setupAnimation(){
-    $('.animated').each(function(){
-      const delay = $(this).data('delay') ;
-      if (delay){
+  function setupAnimation() {
+    $('.animated').each(function () {
+      const delay = $(this).data('delay');
+      if (delay) {
         setTimeout(() => $(this).addClass($(this).data('animate')), delay);
       } else {
         $(this).addClass($(this).data('animate'));
@@ -82,18 +123,18 @@
     });
   }
 
-  function setupAnalytics(){
+  function setupAnalytics() {
     (function (b, o, i, l, e, r) {
       b.GoogleAnalyticsObject = l;
       b[l] || (b[l] =
         function () {
-          (b[l].q = b[l].q || []).push(arguments)
+          (b[l].q = b[l].q || []).push(arguments);
         });
       b[l].l = +new Date;
       e = o.createElement(i);
       r = o.getElementsByTagName(i)[0];
       e.src = 'https://www.google-analytics.com/analytics.js';
-      r.parentNode.insertBefore(e, r)
+      r.parentNode.insertBefore(e, r);
     }(window, document, 'script', 'ga'));
     ga('create', BWOOD.ga);
     ga('send', 'pageview');
